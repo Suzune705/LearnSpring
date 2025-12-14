7b47698d7fd2993d28cb0e803f65d778b37e1bf5
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-12-14 20:37:46 +0700

    refactor : remove user password field

--- src/main/java/com/learnspring/dto/response/UserResponse.java
+++ src/main/java/com/learnspring/dto/response/UserResponse.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Id;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,9 +16,9 @@ import java.time.LocalDate;
 public class UserResponse {
     private int id;
     private String username;
-    private String password;
     private String firstName;
     private String lastName;
     private LocalDate dob;
-}
+    private Set<String> roles ;
+ }
 
