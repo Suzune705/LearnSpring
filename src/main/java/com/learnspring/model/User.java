3e1cc9bb3a84c87ac1cf4ae62bca5ba9427e8a53
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-12-14 20:35:52 +0700

    feat: add user roles

--- src/main/java/com/learnspring/model/User.java
+++ src/main/java/com/learnspring/model/User.java
@@ -1,25 +1,25 @@
 package com.learnspring.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto auto generate ID
     private int id;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
     private LocalDate dob;
+    private Set<String> roles ;
 }
